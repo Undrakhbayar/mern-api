d0c1784e963ddfd4cea289fd5cb6f934b826a9cc
Undrakhbayar <undrakh0511@gmail.com>
2023-02-17 14:52:22 +0800

    a

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'https://mern-7qvv.onrender.com',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://api.gaali.mn'
 ]
 
 module.exports = allowedOrigins
